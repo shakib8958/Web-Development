3db97745e2c8913ca37956b0f79adcc9bdf35b7c
Shakib Ali <113856506+shakib8958@users.noreply.github.com>
2022-10-03 13:13:51 +0530

    Add files via upload

---
+++ Back-end/Demo.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+class demo
+{
+ public static void main(String arg[])
+ {
+ Scanner sc=new Scanner(System.in);
+ 
+ System.out.println("enter Age");
+ int age=sc.nextInt();
+ Sc.nextLine();
+ System.out.println("enter Name");
+ String name=sc.nextLine();
+ System.out.println("salary:"+salary);
+ float sal=sc.nextFloat();
+ System.out.println("Name:"+name);
+ System.out.println("Age:"+age);
+ System.out.println("Salary:"+salary);
+ System.out.println("Hi "+name+"your age is "+age+" and your salary is "+sal);
+ }
+ 
+
+ }
+ 
+ 
+ 
+ 
+ 
