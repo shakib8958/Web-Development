5d4d1d0cc8dc06db664a0c3fead3adfc7356db84
Shakib Ali <113856506+shakib8958@users.noreply.github.com>
2022-10-03 13:15:38 +0530

    Add files via upload

---
+++ Back-end/TwoDArray.java
@@ -0,0 +1,22 @@
+import java.util.*;
+class TwoDArray
+{
+public static void main(String[]args)
+{
+Scanner sc=new Scanner(System.in)
+{
+int rows=sc.nextInt();
+int colum=sc.nextInt();
+int[][] numbers=new int[3][6];
+for(int i=0;i<rows;i++)
+{
+for(int j=0;j<colum;j++)
+{
+number[i][j]=sc.nextInt();
+}
+
+System.out.println(numbers[i][j]+" ");
+}
+System.out.println();
+}
+}
