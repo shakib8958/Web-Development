31f4b6b2401986fd75f60f079af4d53e0d812eff
Shakib Ali <113856506+shakib8958@users.noreply.github.com>
2022-10-03 13:12:57 +0530

    Add files via upload

---
+++ Back-end/Employee.java
@@ -0,0 +1,28 @@
+import java.until.Scanner;
+class Employee
+{
+Scanner Sc=new Scanner(System.in);
+String name[]=new String[3];
+int sal[][]=new int[3][12];
+for(int i=0;i<3;i++)
+{
+ System.out.println("Enter employee name");
+ name[]=Sc.nextLine[];
+ for(int j=0;j<12;j++)
+ {
+  System.out.println("Enter  salary for the month"+(j+1));
+  sal[][]=Sc.nextInt[];
+  {
+  Sc.nextLine[];
+  }
+  for(int i=0;i<3;i++)
+  {
+  System.out.println("name"+name[i]);
+  for(int j=0;j<12;j++)
+  {
+  System.out.println("salary for the month"+(j+1)+" "+sal[i][j]);
+  }
+  }
+  }
+  }
+
