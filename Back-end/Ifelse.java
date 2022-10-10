234a58976c0ef06ee9b98f433f7d9ccd2151502f
Shakib Ali <113856506+shakib8958@users.noreply.github.com>
2022-10-10 18:00:02 +0530

    Add files via upload

---
+++ Back-end/Ifelse.java
@@ -0,0 +1,22 @@
+import java.util.*;
+class Ifelse{
+public static void main(String[]args)
+{
+	System.out.println("eneter the number");
+}
+int s=90;
+if(s<50)
+{
+System.out.println("pass");
+}
+else if(s>=50 &&s<60)
+{
+System.out.println("result is to good");
+}
+else if(s>=70 &&s<100)
+{
+	System.out.println("b grade");
+}
+}
+
+
