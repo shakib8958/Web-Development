cda906cbd98396c87a901b0cbb68d4a98277b067
Shakib Ali <113856506+shakib8958@users.noreply.github.com>
2022-10-25 17:15:03 +0530

    Add files via upload

---
+++ Back-end/Gmail.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+public class Gmal{
+public static void main(String[]args)
+{
+int n=5;
+int m=4;
+//System.out.pritnln("enter the number");
+Scanner sc=new Scanner(System.in);
+//int n=nextInt();
+for(int i=1;i<=n;i++)
+{
+for(int j=1;j<=i-1;j++)
+{
+System.out.print(j+"");
+}
+System.out.println();
+}
+}
+}
+
